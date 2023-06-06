17db2386cadb6ca0f9232a4a5d4e7039de739a17
Jean <jcharaco@unsa.edu.pe>
2023-06-05 21:32:03 -0500

    Ejercicio2g corregido (Completo)

--- ajedrez/Ejercicio2g.py
+++ ajedrez/Ejercicio2g.py
@@ -1,32 +1,16 @@
 from interpreter import draw
 from chessPictures import *
 
-#draw((knight.join(knight.negative())).verticalMirror())
-# version original
-"""draw(knight.inSquare(0).negative().join(knight.inSquare(1)))
-torres = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
-peones = pawn.horizontalRepeat(8)
+#corregido
+torres = square.negative().under(rock).join(square.under(knight)).join(square.negative().under(bishop)).join(square.under(queen)).join(square.negative().under(king)).join(square.under(bishop)).join(square.negative().under(knight)).join(square.under(rock))
+peones = (square.under(pawn).join(square.negative().under(pawn))).horizontalRepeat(4)
 
 fila = (square.join(square.negative())).horizontalRepeat(4)
 espacio = (fila.up(fila.negative())).verticalRepeat(2)
 
-fichas = (torres.under(peones))
-fichasN = (peones.negative().under(torres.negative()))
+fichas = (peones.up(torres))
+fichasN = (torres.negative().up(peones.negative()))
 
 tablero = (fichasN.up(espacio).up(fichas))
 
 draw(tablero)
-"""
-# version mejorada, con piezas en los cuadros
-torres = rock.inSquare(1).join(knight.inSquare(0)).join(bishop.inSquare(1)).join(queen.inSquare(0)).join(king.inSquare(1)).join(bishop.inSquare(0)).join(knight.inSquare(1)).join(rock.inSquare(0))
-peones = (pawn.inSquare(0).join(pawn.inSquare(1))).horizontalRepeat(4)
-
-fila = (square.join(square.negative())).horizontalRepeat(4)
-espacio = (fila.up(fila.negative())).verticalRepeat(2)
-
-fichas = (torres.under(peones))
-fichasN = (peones.negative().under(torres.negative()))
-
-tablero = (fichasN.up(espacio).up(fichas))
-
-draw(tablero) # imprime el tablero
