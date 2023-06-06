8ea40074903e6af9cc44cb4e4b0dfdcec1519b91
JeanChara <jcharaco@unsa.edu.pe>
2023-06-05 19:22:19 -0500

    avance Ejercicio2g

--- ajedrez/Ejercicio2g.py
+++ ajedrez/Ejercicio2g.py
@@ -1,4 +1,12 @@
 from interpreter import draw
 from chessPictures import *
 
-draw((knight.join(knight.negative())).verticalMirror())
+#draw((knight.join(knight.negative())).verticalMirror())
+torres = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
+peones = pawn.horizontalRepeat(8)
+
+fila = (square.join(square.negative())).horizontalRepeat(4)
+espacio = (fila.up(fila.negative())).verticalRepeat(2)
+
+fichas = (peones.under(torres))
+fichasN = (peones.negative().under(torres.negative()))
