7622eebd9bcd10b77a40a9a9ff37da6ed792c5f7
Jean <jcharaco@unsa.edu.pe>
2023-06-05 21:25:36 -0500

    Ejercicio2b corregido (Completo)

--- ajedrez/Ejercicio2b.py
+++ ajedrez/Ejercicio2b.py
@@ -1,4 +1,4 @@
 from interpreter import draw
 from chessPictures import *
 
-draw(knight.up(knight.negative().horizontalMirror()).join(knight.horizontalMirror().under(knight.negative())))
+draw(knight.up(knight.negative().horizontalMirror()).join(knight.negative().up(knight.horizontalMirror())))
